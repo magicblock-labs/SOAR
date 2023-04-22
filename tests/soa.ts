9ce45c945324fd4d1b47bcc6ee157e29c77f200a
k <mja>
2023-04-22 13:49:05 -0700

    name changes

--- tests/soa.ts
+++ tests/soa.ts
@@ -1,12 +1,12 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { Temp } from "../target/types/temp";
+import { Soar } from "../target/types/soar";
 
-describe("temp", () => {
+describe("soa", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.Temp as Program<Temp>;
+  const program = anchor.workspace.Temp as Program<Soar>;
 
   it("Is initialized!", async () => {
     // Add your test here.

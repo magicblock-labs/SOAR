e20cd82049bbe24443c0b58f12683cec7d78f179
k <mja>
2023-04-22 13:49:05 -0700

    name changes

--- programs/soar/src/lib.rs
+++ programs/soar/src/lib.rs
@@ -3,7 +3,7 @@ use anchor_lang::prelude::*;
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
-pub mod temp {
+pub mod soar {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {

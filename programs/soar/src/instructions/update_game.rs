b1a8819c7993da3bdea0387e0de2f703adfd10d6
crustydev <ademola1145@gmail.com>
2023-05-03 19:45:22 +0100

    [WIP]: Instruction handlers

---
+++ programs/soar/src/instructions/update_game.rs
@@ -0,0 +1,39 @@
+use crate::state::*;
+use crate::utils::resize_account;
+use crate::UpdateGame;
+use anchor_lang::prelude::*;
+
+pub fn handler(
+    ctx: Context<UpdateGame>,
+    new_meta: Option<GameMeta>,
+    new_auth: Option<Vec<Pubkey>>,
+) -> Result<()> {
+    let game_account = &mut ctx.accounts.game;
+
+    if let Some(meta) = new_meta {
+        meta.check_field_lengths()?;
+        game_account.set_meta(meta);
+    }
+
+    if let Some(new_auth) = new_auth {
+        let initial_len = game_account.auth.len();
+        let prev_size = Game::calculate_size(initial_len);
+
+        let new_size = prev_size
+            .checked_sub(initial_len * 32)
+            .unwrap()
+            .checked_add(new_auth.len() * 32)
+            .unwrap();
+
+        resize_account(
+            &game_account.to_account_info(),
+            &ctx.accounts.payer.to_account_info(),
+            &ctx.accounts.system_program.to_account_info(),
+            new_size,
+        )?;
+
+        game_account.set_auth(new_auth);
+    };
+
+    Ok(())
+}

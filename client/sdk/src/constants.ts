4fbc0ccbb149d7cafa50065da250244263d3b029
crustydev <ademola1145@gmail.com>
2023-05-09 12:26:43 +0100

    lint: Yarn lint

--- client/sdk/src/constants.ts
+++ client/sdk/src/constants.ts
@@ -1,4 +1,4 @@
 import { PublicKey } from "@solana/web3.js";
 
-export const PROGRAM_ADDRESS = 'Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS'
-export const PROGRAM_ID = new PublicKey(PROGRAM_ADDRESS)
+export const PROGRAM_ADDRESS = "Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS";
+export const PROGRAM_ID = new PublicKey(PROGRAM_ADDRESS);
--- client/sdk/src/utils.ts
+++ client/sdk/src/utils.ts
@@ -1,5 +1,5 @@
 import { PublicKey } from "@solana/web3.js";
-import BN from "bn.js";
+import type BN from "bn.js";
 
 const enum Seeds {
   GAME = "game",
@@ -11,40 +11,64 @@ const enum Seeds {
 
 export const deriveGameAddress = (
   creator: PublicKey,
-  programId: PublicKey,
+  programId: PublicKey
 ): [PublicKey, number] =>
-  PublicKey.findProgramAddressSync([Buffer.from(Seeds.GAME), creator.toBuffer()], programId);
+  PublicKey.findProgramAddressSync(
+    [Buffer.from(Seeds.GAME), creator.toBuffer()],
+    programId
+  );
 
 export const deriveLeaderBoardAddress = (
   id: BN,
   game: PublicKey,
-  programId: PublicKey,
-): [PublicKey, number] => 
-  PublicKey.findProgramAddressSync([Buffer.from(Seeds.LEADER), game.toBuffer(), id.toBuffer('le', 8)], programId);
+  programId: PublicKey
+): [PublicKey, number] =>
+  PublicKey.findProgramAddressSync(
+    [Buffer.from(Seeds.LEADER), game.toBuffer(), id.toBuffer("le", 8)],
+    programId
+  );
 
 export const deriveAchievementAddress = (
   game: PublicKey,
   title: string,
-  programId: PublicKey,
-): [PublicKey, number] => 
-  PublicKey.findProgramAddressSync([Buffer.from(Seeds.ACHIEVEMENT), game.toBuffer(), Buffer.from(title)], programId);
+  programId: PublicKey
+): [PublicKey, number] =>
+  PublicKey.findProgramAddressSync(
+    [Buffer.from(Seeds.ACHIEVEMENT), game.toBuffer(), Buffer.from(title)],
+    programId
+  );
 
-export const derivePlayerInfoAddress= (
+export const derivePlayerInfoAddress = (
   user: PublicKey,
-  programId: PublicKey,
-): [PublicKey, number] => 
-  PublicKey.findProgramAddressSync([Buffer.from(Seeds.PLAYER), user.toBuffer()], programId);
+  programId: PublicKey
+): [PublicKey, number] =>
+  PublicKey.findProgramAddressSync(
+    [Buffer.from(Seeds.PLAYER), user.toBuffer()],
+    programId
+  );
 
 export const derivePlayerEntryListAddress = (
   playerInfo: PublicKey,
   leaderboard: PublicKey,
-  programId: PublicKey,
-): [PublicKey, number] => 
-  PublicKey.findProgramAddressSync([Buffer.from(Seeds.ENTRY), playerInfo.toBuffer(), leaderboard.toBuffer()], programId);
+  programId: PublicKey
+): [PublicKey, number] =>
+  PublicKey.findProgramAddressSync(
+    [Buffer.from(Seeds.ENTRY), playerInfo.toBuffer(), leaderboard.toBuffer()],
+    programId
+  );
 
 export const derivePlayerAchievement = (
   player: PublicKey,
   achievement: PublicKey,
-  programId: PublicKey,
-): [PublicKey, number] => 
-  PublicKey.findProgramAddressSync([player.toBuffer(), achievement.toBuffer()], programId);
+  programId: PublicKey
+): [PublicKey, number] =>
+  PublicKey.findProgramAddressSync(
+    [player.toBuffer(), achievement.toBuffer()],
+    programId
+  );
+
+export const zip = <T, U>(a: T[], b: U[], defaultB: U): Array<[T, U]> =>
+  a.map((k, i) => {
+    if (b.length <= i) return [k, defaultB];
+    return [k, b[i]];
+  });

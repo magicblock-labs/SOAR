1db55efc1b4722da7267b92f11ef3697edaf32c4
Gabriele Picco <piccogabriele@gmail.com>
2023-10-10 17:24:55 +0200

    :art: Code refactor

--- crates/soar-cpi/src/lib.rs
+++ crates/soar-cpi/src/lib.rs
@@ -1,2 +1,4 @@
-clockwork_anchor_gen::generate_cpi_crate!("idl.json");
+clockwork_anchor_gen::generate_cpi_interface!(
+    idl_path = "idl.json"
+);
 declare_id!("SoarmxsvnGcQzLGaiue8mVTko7uuEfTY5zwdKKCnQDU");

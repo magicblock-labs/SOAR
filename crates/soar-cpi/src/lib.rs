4021f2a8719d67fb11978a1a31e4d200218afb2e
Gabriele Picco <piccogabriele@gmail.com>
2023-10-12 12:04:45 +0200

    :green_heart: Fix CI

--- crates/soar-cpi/src/lib.rs
+++ crates/soar-cpi/src/lib.rs
@@ -1,4 +1,5 @@
-clockwork_anchor_gen::generate_cpi_interface!(
-    idl_path = "idl.json"
-);
+use anchor_lang::declare_id;
+use clockwork_anchor_gen::generate_cpi_interface;
+
+generate_cpi_interface!(idl_path = "idl.json");
 declare_id!("SoarmxsvnGcQzLGaiue8mVTko7uuEfTY5zwdKKCnQDU");
